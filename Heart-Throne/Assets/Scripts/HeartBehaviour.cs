088bb3ce45d42b3664d54498851a59980be45a02
Louis Carl Pepin <p.louis4@gmail.com>
2015-04-18 12:05:39 -0700

    Scripts.

---
+++ Heart-Throne/Assets/Scripts/HeartBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartBehaviour : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		{
+			if (other.gameObject.tag == "Floor")
+			{
+				Debug.Log("Your heart touched the floor!");
+			}
+		}
+	}
+}
---
+++ Heart-Throne/Assets/Scripts/ThrowHeartBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowHeartBehaviour : MonoBehaviour {
+
+	public GameObject heart;
+	public int force;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			// Launch heart
+			GameObject heartInst = (GameObject)Instantiate(heart, transform.position, transform.rotation);
+			heartInst.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+		}
+	}
+}
