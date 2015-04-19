4d1732ccc7bd1559d7766425b183f0ce0f904357
Louis Carl Pepin <p.louis4@gmail.com>
2015-04-19 15:15:26 -0700

    Better speech bubble.

--- Heart-Throne/Assets/Scripts/SpeechLook.cs
+++ Heart-Throne/Assets/Scripts/SpeechLook.cs
@@ -12,7 +12,7 @@ public class SpeechLook : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 diff = target.position - transform.position;
+		Vector3 diff = target.position - transform.position + new Vector3(0, 1, 0);
 
 		Quaternion lookat = Quaternion.LookRotation(diff);
 		transform.rotation = Quaternion.Slerp(transform.rotation, lookat, Time.deltaTime * speed);
