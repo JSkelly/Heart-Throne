838cf9ff25f8a51a38ce8f62ba2d421fe7797cc0
Louis Carl Pepin <p.louis4@gmail.com>
2015-04-19 11:58:13 -0700

    Better AI, for car and NPC.

--- Heart-Throne/Assets/Scripts/Wander.cs
+++ Heart-Throne/Assets/Scripts/Wander.cs
@@ -10,9 +10,6 @@ public class Wander : MonoBehaviour
 	public Transform[] waypoints;
 	private int current = 0;
 	public float speed = 3.0f;
-	public float pauseTime = 2.0f;
-	private float timer = 0.0f;
-	private bool pause = false;
 
 	// Use this for initialization
 	void Start()
@@ -34,9 +31,8 @@ public class Wander : MonoBehaviour
 			Vector3 diff = target - transform.position;
 			Vector3 dir = new Vector3(diff.x, 0, diff.z);
 
-			if (dir.magnitude < 1.0f)
+			if (dir.magnitude < 2.0f)
 			{
-				pause = true;
 				current++;
 			}
 			else
@@ -44,16 +40,8 @@ public class Wander : MonoBehaviour
 				Quaternion lookat = Quaternion.LookRotation(dir);
 				transform.rotation = Quaternion.Slerp(transform.rotation, lookat, Time.deltaTime);
 
-				timer += Time.deltaTime;
-				if (pause && timer > pauseTime)
-				{
-					timer = 0.0f;
-					pause = false;
-				}
-				else if(!pause)
-				{
-					GetComponent<CharacterController>().Move(transform.forward.normalized * speed * Time.deltaTime);
-				}
+				float actualSpeed = Mathf.Abs(Quaternion.Angle(transform.rotation, lookat)) < 45.0f ? speed : speed / 5;
+				GetComponent<CharacterController>().Move(transform.forward.normalized * actualSpeed * Time.deltaTime);
 			}
 		}
 		else
