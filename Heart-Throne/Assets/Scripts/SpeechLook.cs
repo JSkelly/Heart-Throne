0061872860630d087f11c881a90b6819c90e8638
Louis Carl Pepin <p.louis4@gmail.com>
2015-04-19 15:03:35 -0700

    Speech bubble.

---
+++ Heart-Throne/Assets/Scripts/SpeechLook.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeechLook : MonoBehaviour {
+	public Transform target;
+	public float speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 diff = target.position - transform.position;
+
+		Quaternion lookat = Quaternion.LookRotation(diff);
+		transform.rotation = Quaternion.Slerp(transform.rotation, lookat, Time.deltaTime * speed);
+	}
+}
